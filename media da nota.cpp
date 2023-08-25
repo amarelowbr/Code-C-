1ce8bf48f6433a721094ea14b1a5c326392df05c
amarelowbr <143194024+amarelowbr@users.noreply.github.com>
2023-08-25 15:36:22 -0300

    Add files via upload

---
+++ media da nota.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+float nota1, nota2, media;
+
+printf("digite sua primeira nota:");
+scanf_s("%f",&nota1);
+
+printf("digite sua segunda nota:");
+scanf_s("%f",&nota2);
+media=(nota1+nota2)/2;
+printf("sua media e : %0.f",media);
+
+
+
+}
